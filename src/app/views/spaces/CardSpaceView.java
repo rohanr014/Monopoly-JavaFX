a7d5916f8c5c20eaa9450f4818964364e294e097
Jaiveer Katariya <jk386@duke.edu>
2019-04-17 00:48:07 -0400

    added rohans images

--- src/app/views/spaces/CardSpaceView.java
+++ src/app/views/spaces/CardSpaceView.java
@@ -29,7 +29,7 @@ public class CardSpaceView extends SpaceView<CardSpace> {
     @Override
     public void initialize() {
         myRoot = new StackPane();
-        tempCard = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream("monopoly-community-chest-clipart-1.jpg")));
+        tempCard = new ImageView(new Image(this.getClass().getClassLoader().getResourceAsStream("community-chest-space.jpg")));
         tempCard.setFitWidth(40);
         tempCard.setFitHeight(40);//need to not hard code this
         myRoot.getChildren().addAll(tempCard);
